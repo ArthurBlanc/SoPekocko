fb1e567bdb308c0e276b1f6db4f5c10b4d619b6a
Arthur BLANC <72075460+ArthurBlanc@users.noreply.github.com>
2021-02-09 11:03:04 +0100

    fix errors

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,7 +9,7 @@ module.exports = (req, res, next) => {
 		const token = req.headers.authorization.split(" ")[1];
 		const decodedToken = jwt.verify(token, `${process.env.JWT_SECRET_KEY}`);
 		const userId = decodedToken.userId;
-		if (req.body.userId !== userId) {
+		if (req.body.userId && req.body.userId !== userId) {
 			throw "User ID non valable !";
 		} else {
 			next();
